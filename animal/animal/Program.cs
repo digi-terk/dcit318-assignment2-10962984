fa1e97d529d09f3d06c3acfb0c1f44a66b753295
SUMAILA ASARA DASMANI <sadasmani@st.ug.edu.gh>
2024-07-06 09:00:39 +0100

    Completed animal class

---
+++ animal/animal/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OOPDemo
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Another derived class
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound();  // Output: Some generic sound
+            dog.MakeSound();            // Output: Bark
+            cat.MakeSound();            // Output: Meow
+        }
+    }
+}
+
+
